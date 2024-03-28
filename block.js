b3efabf327bf9113564da505fa4829bff3d58f34
Tanishk Agrawal <tanishkagrawal241@gmail.com>
2024-03-28 09:46:49 +0530

    Update block.js

--- block.js
+++ block.js
@@ -1,6 +1,6 @@
 const hexToBinary = require("hex-to-binary");
 const { GENESIS_DATA, MINE_RATE} = require("./config");
-const cryptoHash = require("./crypto-hash");
+const cryptoHasH = require("./crypto-hash");
 
 class Block {
     constructor({ timestamp, prevHash, hash, data,nonce,difficulty}) {
